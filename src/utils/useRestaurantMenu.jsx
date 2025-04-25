a6a8471713fe273ff1c846ad2eed816eee2c894a
ashutosh sahu <ppaskumar@gmail.com>
2025-04-25 08:40:15 +0530

    error solve of await

--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -9,13 +9,17 @@ const useRestaurantMenu = (resId) => {
 
   const fetchData = async () => {
     // const data = await fetch(MENU_API + resId);
-    // const data = await fetch("http://localhost:3001/menu");
+
     // const data = await fetch(
     //   `http://localhost:3001/menu?restaurantId=${resId}`
     // );
-    const data = fetch(
+    const data = await fetch(
       `https://cart-karo-backend.vercel.app/menu?restaurantId=${resId}`
     );
+
+    // const data = await fetch(
+    //   "https://cart-karo-backend.vercel.app/menu?restaurantId=12345"
+    // );
     const json = await data.json();
     setRestInfo(json.data);
   };
