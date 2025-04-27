326be613a4ecb7df42d86b29c9c941b404afdca9
ashutosh sahu <ppaskumar@gmail.com>
2025-04-27 05:42:06 +0530

    padding changed for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   const cartItems = useSelector((store) => store.cart.items);
 
   return (
-    <div className="flex justify-between shadow-lg py-3 px-2 md:px-10 sticky top-0 z-[1000] bg-white">
+    <div className="flex justify-between shadow-lg py-3 px-6 md:px-10 sticky top-0 z-[1000] bg-white">
       <SiSwiggy className="p-2 bg-[#ff5200] rounded-xl text-white text-5xl" />
 
       <div className="flex items-center text-gray-700">
@@ -28,7 +28,6 @@ const Header = () => {
           <li className="font-bold text-xl ">
             <Link to="/cart"> Cart - ({cartItems.length} items)</Link>
           </li>
-          {/* <li className="font-bold text-xl hidden lg:flex">{loggedInUser}</li> */}
         </ul>
       </div>
     </div>
